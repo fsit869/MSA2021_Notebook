2eacf040d84a87dbeaa37b552315b9841836df99
Frank Situ <62918263+pengu1976@users.noreply.github.com>
2021-08-06 17:26:41 +1200

    Created plan doc. Deleted template code from create-react-app and storybook

--- frontend/msa-notebook/src/App.tsx
+++ frontend/msa-notebook/src/App.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+class App extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1> My website </h1>
         <Button variant="contained" color="primary">Materials UI test</Button>
-      </header>
-    </div>
-  );
+      </React.Fragment>
+      
+    )
+  }
 }
 
 export default App;
