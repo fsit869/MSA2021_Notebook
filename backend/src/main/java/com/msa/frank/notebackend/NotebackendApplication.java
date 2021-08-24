16a44e08c4e5c45fff30777419ea37bc70a70eec
fsit869 <62918263+fsit869@users.noreply.github.com>
2021-08-25 00:05:06 +1200

    Created dependencies for backend.
    Backend dependencies and project created.

---
+++ backend/src/main/java/com/msa/frank/notebackend/NotebackendApplication.java
@@ -0,0 +1,13 @@
+package com.msa.frank.notebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/msa/frank/notebackend/NotebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.frank.notebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
